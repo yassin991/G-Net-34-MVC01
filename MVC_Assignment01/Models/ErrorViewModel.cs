b0f4caf1b6c93a946b4652174dd94059eba2ad26
yassin991 <yassenelsherbiny69@gmail.com>
2026-05-13 11:48:11 +0300

    Add project files.

---
+++ MVC_Assignment01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Assignment01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
